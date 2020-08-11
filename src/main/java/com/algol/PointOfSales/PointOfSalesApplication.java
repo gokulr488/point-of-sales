5d016febadef9305a15c4ba0e909fa1c0ccdc89d
gokul Laptop <gokulr488@gmail.com>
2020-08-11 14:38:18 +0530

    zx

--- src/main/java/com/algol/PointOfSales/PointOfSalesApplication.java
+++ src/main/java/com/algol/PointOfSales/PointOfSalesApplication.java
@@ -18,26 +18,28 @@ public class PointOfSalesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PointOfSalesApplication.class, args);
 	}
+
 	@Bean
-    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
- @Bean
- public CommonsRequestLoggingFilter requestLoggingFilter() {
-     CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-     loggingFilter.setIncludeClientInfo(true);
-     loggingFilter.setIncludePayload(true);
-     loggingFilter.setMaxPayloadLength(64000);
-     return loggingFilter;
- }
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
+	}
 
 }
